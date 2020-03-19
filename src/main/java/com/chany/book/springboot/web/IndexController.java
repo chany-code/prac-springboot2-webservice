9f778ec744be811f9c743870c87740f381b621e9
chany <channn926@gmail.com>
2020-03-19 10:59:28 +0900

    chapter5-2 Google 로그인 개선

--- src/main/java/com/chany/book/springboot/web/IndexController.java
+++ src/main/java/com/chany/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.chany.book.springboot.web;
 
+import com.chany.book.springboot.config.auth.LoginUser;
 import com.chany.book.springboot.config.auth.dto.SessionUser;
 import com.chany.book.springboot.service.posts.PostsService;
 import com.chany.book.springboot.web.dto.PostsResponseDto;
@@ -9,19 +10,17 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
     //전체 UI만들면서 추가된 부분
     private final PostsService postsService;
-    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){ //서버템플릿엔진에서 사용할 수 있는 객체를 저장할 수 있음. postsService.findAllDesc()로 가져온 결과를 posts로 index. mustache에 전달
+    public String index(Model model, @LoginUser SessionUser user){ //서버템플릿엔진에서 사용할 수 있는 객체를 저장할 수 있음. postsService.findAllDesc()로 가져온 결과를 posts로 index. mustache에 전달
         model.addAttribute("posts",postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        //SessionUser user = (SessionUser) httpSession.getAttribute("user"); ***@LoginUser로 개선함으로서 사라진 부분
         if(user!=null){
             model.addAttribute("userName", user.getName());
         }
@@ -37,7 +36,7 @@ public class IndexController {
         //src/main/resources/templates/index.mustache로 전환되어 View Resolver가 처리함
         // *View Resolver는 URL 요청의 결과를 전달할 타입과 값을 지정하는 관리자 격
     }  ***전체UI만들기 전에 사용했던 부분***
-     */
+    */
 
     @GetMapping("/posts/save")//이건살아있어야지
     public String postsSave(){
@@ -48,7 +47,6 @@ public class IndexController {
     public String postsUpdate(@PathVariable Long id, Model model){
         PostsResponseDto dto = postsService.findById(id);
         model.addAttribute("post", dto);
-
         return "posts-update";
     }
 
